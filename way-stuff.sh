34b2a15cfdc05dc845546e10f390c8ab3c46cc8b
Friendly Alien <164930880+fryalien@users.noreply.github.com>
2025-07-11 11:58:59 +0200

    y

--- way-stuff.sh
+++ way-stuff.sh
@@ -1,4 +1,4 @@
-sudo pacman -S --noconfirm --needed nm-applet
+sudo pacman -S --noconfirm --needed network-manager-applet
 sudo pacman -S --noconfirm --needed pipewire
 sudo pacman -S --noconfirm --needed polkit-gnome-agent
 sudo pacman -S --noconfirm --needed seatd
@@ -15,6 +15,7 @@ sudo pacman -S --noconfirm --needed xdg-desktop-portal-gtk
 sudo pacman -S --noconfirm --needed xdg-desktop-portal-kde
 sudo pacman -S --noconfirm --needed xdg-desktop-portal-wlr
 sudo pacman -S --noconfirm --needed xdg-desktop-portal-xapp
+sudo pacman -S --noconfirm --needed xwayland-satellite
 
 # can't install from aur, error status 4
 # yay -S --noconfirm --needed wdisplays
